91c3c4c51080d563ab4dc5452f5d7503964f1f12
sav21age <sav21age@gmail.com>
2023-06-10 08:57:01 +0300

    Edit admin form fields

--- images/admin.py
+++ images/admin.py
@@ -1,10 +1,17 @@
 from django.contrib.contenttypes.admin import GenericStackedInline
 from images.forms import ImageAdminForm
 from images.models import Image
+from django.forms import TextInput, Textarea
+from django.db import models
 
+formfield_overrides = {
+    models.CharField: {'widget': TextInput(attrs={'style': 'width: 70%; font-size: 115%;'})},
+    models.TextField: {'widget': Textarea(attrs={'rows': 30, 'style': 'width: 70%; font-size: 115%;'})},
+}
 
 class ImageInline(GenericStackedInline):
     model = Image
     form = ImageAdminForm
     extra = 0
     show_change_link = True
+    formfield_overrides = formfield_overrides
--- videos/admin.py
+++ videos/admin.py
@@ -1,9 +1,18 @@
 from django.contrib.contenttypes.admin import GenericStackedInline
 from videos.forms import VideoAdminForm
 from videos.models import Video
+from django.forms import TextInput, Textarea
+from django.db import models
+
+
+formfield_overrides = {
+    models.CharField: {'widget': TextInput(attrs={'style': 'width: 70%; font-size: 115%;'})},
+    models.TextField: {'widget': Textarea(attrs={'rows': 30, 'style': 'width: 70%; font-size: 115%;'})},
+}
 
 class VideoInline(GenericStackedInline):
     model = Video
     form = VideoAdminForm
     extra = 0
     show_change_link = True
+    formfield_overrides = formfield_overrides
