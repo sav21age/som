9045d664e644940817b0aa8cd5c1e3cf70db400c
sav21age <sav21age@gmail.com>
2023-09-21 18:08:49 +0300

    Fix aos animation

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -302,14 +302,14 @@
   /**
    * Animation on scroll
    */
-  window.addEventListener("load", () => {
+  // window.addEventListener("load", () => {
     AOS.init({
       duration: 1000,
       easing: "ease-in-out",
       once: true,
       mirror: false,
     });
-  });
+  // });
 
   // --
 
